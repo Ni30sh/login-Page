75dc1ad689c9754de7dee94e7368786824b54dfa
Ni30shkumar <addy2166@gmail.com>
2023-07-16 16:29:39 +0530

    c commit

--- loginpage.py
+++ loginpage.py
@@ -5,4 +5,6 @@ log.geometry("500x500+300+120")
 
 login_frame = Label(log, text="Sign",font=("Microsoft YaHei UI Light", 12, "italic bold"))
 login_frame.place(x=250,y=40,height=40,width=60)
+
+
 log.mainloop()
